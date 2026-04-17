5b756d3586efb361b3c006d57927840cb961c9bc
atharv deshmukh <atharvd.2706@gmail.com>
2026-04-17 13:38:20 +0530

    Add files via upload

---
+++ exp.1.1.py
@@ -0,0 +1,9 @@
+# Program to display personal details
+
+name = "Your Name"
+address = "Your Address"
+contact_number = "1234567890"
+
+print("Name:", name)
+print("Address:", address)
+print("Contact Number:", contact_number)
